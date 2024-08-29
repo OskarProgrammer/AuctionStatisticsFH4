e3a6eff629b479370c95dfdebc5a105ca21be6a3
Oskar <Zaden>
2024-08-29 23:03:09 +0200

    Updating App.jsx creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 //layouts 
 import HomeLayout from './layouts/HomeLayout'
 
+//pages
+import MainPage, { mainPageLoader } from './pages/MainPage'
+import LoginPage, { loginAction } from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { LogOutPage, logOut } from './pages/LogOutPage'
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout/>} >
-        <Route index element={<MainPage/>} />
-
+      <Route index element={<MainPage />} loader={mainPageLoader}/>
+      <Route path="/login" element={<LoginPage/>} action={loginAction}/>
+      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/logOut" element={<LogOutPage/>} laoder={logOut}/>
     </Route>
   )
 )
