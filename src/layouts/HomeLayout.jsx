2cb9b67000bf1f5718ba2427338ec2206a2d6734
Oskar <Zaden>
2024-08-29 23:02:23 +0200

    Creating HomeLayout.jsx'

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,10 +1,16 @@
+import { Outlet } from "react-router-dom"
 
 
 const HomeLayout = () => {
 
     return (
         <>
-            HOMELAYOUT
+            <header>
+                <h1 className="mx-auto mt-5 display-4 col-lg-6 col-md-6 col-sm-6 col-10 text-center fw-bold p-5 bg-light rounded">
+                        Auction statistics manager
+                </h1>
+            </header>
+            <Outlet/>
         </>
     )
 }
