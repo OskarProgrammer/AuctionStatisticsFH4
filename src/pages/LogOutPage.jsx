64a62deb7a56e38628ff54b887a00b261374ec2d
Oskar <Zaden>
2024-08-29 22:57:23 +0200

    Creating LogOutPage.jsx

---
+++ src/pages/LogOutPage.jsx
@@ -0,0 +1,24 @@
+import { redirect, useLoaderData } from "react-router-dom"
+import { putRequest } from "../API/requests.js"
+
+export const LogOutPage = () => {
+    const loaderData = useLoaderData()
+    redirect("/")
+
+    return(<></>)
+}
+
+export const logOut = async () => {
+    let newCurrent = {
+        id:"",
+        isLogged: false
+    }
+
+    try {
+        await putRequest("http://localhost:3000/currentUser/", newCurrent)
+    } catch {
+        throw new Error("Logging out error")
+    }
+
+    return redirect("/")
+}
