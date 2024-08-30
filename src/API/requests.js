68c9e819bba5b2bc1abbb6b3c95afadcfbcb441f
Oskar <Zaden>
2024-08-30 10:59:39 +0200

    Creating postRequest

--- src/API/requests.js
+++ src/API/requests.js
@@ -11,9 +11,35 @@ export const getRequest = async (endpoint) => {
     return data.json()
 }
 
+export const getRequestId = async (endpoint, id) => {
+    let data = []
+
+    try{
+        data = await fetch (endpoint+id)
+    } catch {
+        throw new Error("Get request went wrong")
+    }
+
+    return data.json()
+}
+
 export const putRequest = async (endpoint, payload) => {
     const requestOptions = {
         method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    }
+
+    try {
+        await fetch(endpoint, requestOptions)
+    } catch {
+        throw new Error("Put request went wrong")
+    }
+}
+
+export const postRequest = async (endpoint, payload) => {
+    const requestOptions = {
+        method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
     }
