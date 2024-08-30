51050423581e730fb19298b46a620f45bc6e3e74
Oskar <Zaden>
2024-08-30 21:02:30 +0200

    Fixing some conditional rendering

--- src/components/HistoryTab.jsx
+++ src/components/HistoryTab.jsx
@@ -5,8 +5,9 @@ export const HistoryTab = ( props ) => {
     return(
         <div className={`container ${auction.status == "success" ? "bg-success": "bg-danger"} text-white rounded border-dark border-2 p-3 my-3`}>
             <h2 className="display-4 fw-bold">{auction.nameOfCar}</h2>
-            {auction.status == "success" ? <p className="fs-4">Sold for {auction.finalPrice}</p> : ""}
-            {auction.finalPrice == auction.buyoutCost ? <p className="fs-4">Method of purchase: buyout</p> : <p className="fs-4">Method of purchase: bidding</p>}
+            {auction.status == "success" ? <p className="fs-4">Sold for {auction.finalPrice}</p> : <p className="fs-4">Not sold</p>}
+            {auction.status != "failure" ? (auction.finalPrice == auction.buyoutCost ? <p className="fs-4">Method of purchase: buyout</p> 
+            : <p className="fs-4">Method of purchase: bidding</p>) : ""}
         </div>
     )
 }
