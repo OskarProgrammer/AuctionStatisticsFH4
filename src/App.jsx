a836b45bb4b6bbdeac623c963f2625ba8b81107b
Oskar <Zaden>
2024-08-30 11:22:35 +0200

    Making some code stylizing to increase readbility

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,44 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 //layouts 
 import HomeLayout from './layouts/HomeLayout'
+import { AccountLayout } from './layouts/AccountLayout'
 
 //pages
 import MainPage, { mainPageLoader } from './pages/MainPage'
 import LoginPage, { loginAction } from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import { LogOutPage, logOut } from './pages/LogOutPage'
-import { AccountLayout } from './layouts/AccountLayout'
 import { AccountDetails, accountDetailsLoader } from './pages/AccountDetails'
 import { newAuctionAction, NewAuctionPage } from './pages/NewAuctionPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout/>} >
-      <Route index element={<MainPage />} loader={mainPageLoader}/>
-      <Route path="/login" element={<LoginPage/>} action={loginAction}/>
-      <Route path="/register" element={<RegisterPage/>}/>
+
+      <Route index element={<MainPage />} 
+                   loader={mainPageLoader}/>
+
+      <Route path="/login" 
+             element={<LoginPage/>} 
+             action={loginAction}/>
+
+      <Route path="/register" 
+             element={<RegisterPage/>}/>
       
       <Route path="/account/" element={<AccountLayout/>} >
-        <Route path="details" element={<AccountDetails/>} loader={accountDetailsLoader}/>
-        <Route path="logOut" element={<LogOutPage/>} loader={logOut} />
-        <Route path="newAuction" element={<NewAuctionPage/>} action={newAuctionAction}/>
+
+          <Route  path="details" 
+                  element={<AccountDetails/>} 
+                  loader={accountDetailsLoader}/>
+
+          <Route  path="logOut" 
+                  element={<LogOutPage/>} 
+                  loader={logOut} />
+
+          <Route path="newAuction" 
+                 element={<NewAuctionPage/>} 
+                 action={newAuctionAction}/>
+
       </Route>
     </Route>
   )
